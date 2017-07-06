c0380de35b29c158e3a76ea53815900c6a6ac937
ayoub <ayoub-chakri@hotmail.com>
2017-07-06 17:50:49 +0100

    v1

---
+++ pages/home/about.vue
@@ -0,0 +1,12 @@
+<template lang="html" >
+  <h1> Home/about </h1>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style lang="css" >  
+</style>
---
+++ pages/home/index.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="container" >
+        <h2>liste des Produits : </h2>
+        <div v-for="product in products" >
+            <div class="panel panel-default">
+                <div class="panel-heading"></div>
+                <div class="panel-body">
+                    <h5> {{ product.name }} {{ product.slug }} </h5>
+                    <span>type : {{ product.type }} </span><br>
+                    <span>Prix : {{ product.sale_price }} </span><br>
+                </div>
+                <div class="panel-footer"></div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios';
+
+    export default {
+   
+        asyncData ({ params }) {
+    
+            return axios.get('http://127.0.0.1:8000/api/products/get_all')
+                        .then((response) => {
+                            return { products : response.data }    
+                        })
+            ; 
+
+        }
+
+    }
+</script>
+
+
+<style lang="css" >
+  @import url(/bootstrap.min.css);
+</style>
